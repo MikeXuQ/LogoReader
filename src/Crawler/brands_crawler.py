16001964f799cc4b97935f3673ce028f265833e6
Dimitri <791219648@qq.com>
2017-08-29 13:00:04 +0800

    try collecting brands from tmall.com but fail

--- src/Crawler/brands_crawler.py
+++ src/Crawler/brands_crawler.py
@@ -1,7 +1,6 @@
 # -*- coding:utf-8 -*-
 
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 
 class BrandsCrawler(object):
     def __init__(self, url, debug=False):
@@ -9,30 +8,22 @@ class BrandsCrawler(object):
         self.debug = debug
 
     def get_brands(self):
-        brands = []
+        self.brands = []
         if self.debug:
             self.driver = webdriver.Chrome()
         else:
             self.driver = webdriver.PhantomJS()
         self.driver.get(self.url)
-        category = self.driver.find_element_by_class_name("nav-line-2")
-        hover = ActionChains(self.driver).move_to_element(category)
-        hover.perform()
-
-        panels = self.driver.find_elements_by_class_name('nav-hasPanel')
-        # 从第4个panel开始遍历，首先收集每个分类的链接
-        category_links = [link.get_attribute('href') \
-            for i in range(3, len(panels)) \
-                for link in panels[i].find_elements_by_tag_name('a')]
-        print 'url', category_links
-        for category_link in category_links:
-            self.driver.get(category_link)
-            tmp_element = [x for x in self.driver.find_elements_by_tag_name('h3') if x.text == u'品牌'][0]
-            brands += [x.text for x in\
-                tmp_element.find_elements_by_xpath('following-sibling::ul//a')]
-            print brands
+        categories = [x.text for x in \
+            self.driver.find_elements_by_xpath('//li[contains(@class, \'j_MenuNav\')]//a')]
+        for category in categories:
+            self.driver.find_element_by_name('q').send_keys(category)
+            self.driver.find_element_by_xpath('//button[@type=\'submit\']').click()
+            print self.driver.current_url
+            self.brands += [x.title for x in \
+                self.driver.find_elements_by_xpath('//ul[contains(@class, \'av-collapse\')]//a')]
 
 if __name__ == '__main__':
-    url = 'https://www.amazon.cn'
-    crawler = BrandsCrawler(url, debug=True)
+    url = 'https://www.tmall.com'
+    crawler = BrandsCrawler(url, debug=False)
     crawler.get_brands()
