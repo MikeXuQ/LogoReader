e145a523204afa1acddd4a9ffbf2dfff2898d671
Dimitri <791219648@qq.com>
2017-08-30 15:26:00 +0800

    succeed in searching brands from jd.com with requests module

--- src/Crawler/brands_crawler.py
+++ src/Crawler/brands_crawler.py
@@ -1,39 +1,43 @@
 # -*- coding:utf-8 -*-
 
-from selenium import webdriver
+import requests
+from lxml import etree
+import os
 
 class BrandsCrawler(object):
-    def __init__(self, url, debug=False):
-        self.url = url
-        self.debug = debug
+    def __init__(self):
+        self.category_url = 'https://www.jd.com'
+        self.search_url_base = 'https://search.jd.com/Search?enc=utf-8&keyword='
 
-    def get_brands(self):
-        self.brands = []
-        if self.debug:
-            self.driver = webdriver.Chrome()
+    def get_brands(self, file_path):
+        brands = []
+        if os.path.exists(file_path):
+            print 'Getting brands from file...'
+            with open(file_path, 'r') as f:
+                for line in f.readlines():
+                    brands.append(line.strip())
         else:
-            self.driver = webdriver.PhantomJS()
-        self.driver.get(self.url)
-        categories = [x.text for x in \
-            self.driver.find_elements_by_xpath('//li[contains(@class, \'cate_menu_item\')]//a')]
-        for category in categories:
-            search_blank = self.driver.find_element_by_id('key')
-            search_blank.clear()
-            search_blank.send_keys(category)
-            self.driver.find_element_by_xpath('//button[contains(@class, \'button\')]').click()
-            print 'Searching category "%s"' % category.encode('utf-8')
-            # 处理特殊页面导致的异常
-            try:
-                to_add = [x.get_attribute('title').encode('utf-8') for x in \
-                    self.driver.find_elements_by_xpath('//div[contains(@class, \'s-brand\')]//ul[contains(@class, \'v-fixed\')]//a')]
-                print 'Brands to add:\n', to_add
-                self.brands += to_add
-            except Exception, e:
-                print 'Fail to collect brands of "%s" due to %s' % (category, e)
+            print 'Getting brands from pages...'
+            categories = self._get_categories()
+            with open(file_path, 'w') as f:
+                for category in categories:
+                    print 'Searching brands for "%s"...' % category
+                    try:
+                        page = requests.get(self.search_url_base + category, timeout=8).content
+                        html = etree.HTML(page)
+                        to_add = [x.encode('utf-8') for x in \
+                            html.xpath('//div[contains(@class, \'s-brand\')]//ul[contains(@class, \'v-fixed\')]//a/@title')]
+                        brands += to_add
+                        for brand in to_add:
+                            f.write(brand + '\n')
+                    except Exception, e:
+                        print 'Fail to get brands of "%s" due to %s' % (category, e)
 
-        self.driver.close()
+        return brands
 
-if __name__ == '__main__':
-    url = 'https://www.jd.com'
-    crawler = BrandsCrawler(url, debug=False)
-    crawler.get_brands()
+    def _get_categories(self):
+        print 'Collecting categories...'
+        page = requests.get(self.category_url).content
+        html = etree.HTML(page)
+        return [x.encode('utf-8') for x in \
+            html.xpath('//li[contains(@class, \'cate_menu_item\')]//a/text()')]
--- src/Crawler/runner.py
+++ src/Crawler/runner.py
@@ -1,9 +1,11 @@
 from images_crawler import ImagesCrawler
+from brands_crawler import BrandsCrawler
 from multiprocessing import Pool
 
 URL_BASE = "https://image.baidu.com/search/flip?tn=baiduimage&ie=utf-8&pn=0&gsm=50&ct=&ic=0&lm=-1&width=0&height=0&word=word"
 NUM_SEARCH_PAGES = 3
 LOGOS_DIR = '../../data/logos'
+BRANDS_DIR = '../../data/brands'
 
 NUM_PROCESSES = 4
 
@@ -13,6 +15,7 @@ def search(word):
     crawler.close()
 	
 if __name__ == '__main__':
-    to_search = ['acer', 'kindle', 'nike', 'benz', 'beats', 'casio']
+	brands_crawler = BrandsCrawler()
+    to_search = brands_crawler.get_brands(BRANDS_DIR)
     pool = Pool(processes=NUM_PROCESSES)
     pool.map(search, to_search)
