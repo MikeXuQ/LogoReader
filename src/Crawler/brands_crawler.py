d7360d10056a923ccc1e39955bd4865be44543d0
Dimitri <791219648@qq.com>
2017-08-30 13:05:27 +0800

    try jd.com but encounter confusing situation

--- src/Crawler/brands_crawler.py
+++ src/Crawler/brands_crawler.py
@@ -15,15 +15,25 @@ class BrandsCrawler(object):
             self.driver = webdriver.PhantomJS()
         self.driver.get(self.url)
         categories = [x.text for x in \
-            self.driver.find_elements_by_xpath('//li[contains(@class, \'j_MenuNav\')]//a')]
+            self.driver.find_elements_by_xpath('//li[contains(@class, \'cate_menu_item\')]//a')]
         for category in categories:
-            self.driver.find_element_by_name('q').send_keys(category)
-            self.driver.find_element_by_xpath('//button[@type=\'submit\']').click()
-            print self.driver.current_url
-            self.brands += [x.title for x in \
-                self.driver.find_elements_by_xpath('//ul[contains(@class, \'av-collapse\')]//a')]
+            search_blank = self.driver.find_element_by_id('key')
+            search_blank.clear()
+            search_blank.send_keys(category)
+            self.driver.find_element_by_xpath('//button[contains(@class, \'button\')]').click()
+            print 'Searching category "%s"' % category.encode('utf-8')
+            # 处理特殊页面导致的异常
+            try:
+                to_add = [x.get_attribute('title').encode('utf-8') for x in \
+                    self.driver.find_elements_by_xpath('//div[contains(@class, \'s-brand\')]//ul[contains(@class, \'v-fixed\')]//a')]
+                print 'Brands to add:\n', to_add
+                self.brands += to_add
+            except Exception, e:
+                print 'Fail to collect brands of "%s" due to %s' % (category, e)
+
+        self.driver.close()
 
 if __name__ == '__main__':
-    url = 'https://www.tmall.com'
+    url = 'https://www.jd.com'
     crawler = BrandsCrawler(url, debug=False)
     crawler.get_brands()
